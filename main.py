abeff568a4de9a8c7fb41a2acdd0cde57abeaf7b
salmarosidah <143119980+salmarosidah@users.noreply.github.com>
2023-08-28 15:13:57 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,72 @@
+feeling = input('How are you?')
+if feeling.lower() == 'great':
+    print('I feel great too.')
+else :
+    print('I hope the rest of your day is good.')
+    
+    
+    
+    
+    
+    
+m = ['Alice','ants','Bob','Carol','cats']
+m.sort()
+print(m)
+spam = ['a','z','A','Z']
+spam.sort(key=str.lower)
+print(spam)
+
+
+
+from operator import*
+a = 1 
+b = 5.0
+print ('a = {}'.format(a))
+print ('b = {}'.format(b))
+for func in (lt, le, eq, ne, ge, gt):
+    print('{}(a, b): {}'.format(func.__name__, func(a,b)))
+    
+    
+    
+str = "Py'thon"
+str
+str2 = 'Py"thon'
+str2
+"\OK, \" sampai ketemu lagi."
+teks = "Phyton adalah bahasa pemrograman yang powerfull.\nTerbukti Phyton bisa dijalankan di segala platform OS.\nJadi,saatnya kita menggunakan Phyton sebagai bahasa pemrograman \nsehari-hari.\nSalam Phyton Dahsyat!"
+print(teks)
+kata = """Phyton adalah bahasa pemrograman yang powerfull.
+... Terbukti Phyton bisa dijalankan di segala platform OS.
+... Jadi, saatnya kita menggunakan Phyton sebagai bahasa pemrograman
+... sehari-hari. Salam Phyton Dahsyat!"""
+print(kata)
+
+
+blog = 'Klinik' + 'Pyhton'
+print(blog) 
+newblog = blog*5
+print(newblog)
+blog *= 4
+print(blog)
+string = 'Klinik''Phyton'
+print(string)
+buah = 'Nanas'
+print(buah)
+print(buah[0])
+print(buah[0:2])
+print(buah[0:4])
+print(buah[0:5])
+
+
+
+a = "Dear Alice"
+
+
+
+
+
+
+
+
+
+
